8e857d5642944f147cd4c5ccf6835ddfe982426f
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-06-22 15:06:15 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,11 @@ def reverse_each_word(string)
       words.join(" ")
 end
 
+def reverse_each_word(string)
+  words = []
+  array =  string.split(" ")
+  array.collect do |word|
+  words <<  word.reverse
+  end
+      words.join(" ")
+end
